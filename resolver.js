b88b77324ba056059ae12414b8e489377ef4348f
Glitch (harmless-military-blob) <none>
2020-08-26 16:59:45 +0000

    🐊👡 Checkpoint ./resolver.js:9085975/13

--- resolver.js
+++ resolver.js
@@ -1,7 +1,9 @@
-// ANCHOR we define the resolvers which are used as routes and controllers 
+'use strict';
+
+// ANCHOR we define the resolvers which are used as routes and controllers
 const root = {
-    hello: () => "Hello Worlds",
-    // NOTE Code goes here...
+  hello: () => 'Hello Worlds',
+  // NOTE Code goes here...
 };
 
-module.exports =  root;
+module.exports = root;
