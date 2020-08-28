bd6e3237ead1574d085c5f3a12d654d9889ff173
Glitch (harmless-military-blob) <none>
2020-08-28 16:21:00 +0000

    📡🎢 Checkpoint ./src/index.js:9085975/244 ./.eslintrc.json:9085975/14

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 'use strict'
 
 const express = require('express')
+const expressGraphQL = require('express-graphql')
+
 
 const app = express()
+
+app.use('/graphql', expressGraphQL({}))
 
 app.listen(3000, () => console.log('Server Running'))
