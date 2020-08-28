67dc362b31514383875da2c094ea50521f5feee6
Glitch (harmless-military-blob) <none>
2020-08-28 15:58:10 +0000

    🕹🐕 Checkpoint ./package.json:9085975/64 ./src/index.js:9085975/800

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
-'use strict';
+'use strict'
 
-// ANCHOR initialize the server and graphql, set routes and port for the server
-const express = require('express');
-const expressGraphQl = require('express-graphql');
-const Schema = require('./schema');
-const root = require('./resolver');
+const express = require('express')
 
-// Initialize Express server
 const app = express()
 
-// Set Routes
-app.get('/', (req, res) => {
-	return res.json({
-		msg: 'Welcome to my GQL World'
-	})
-})
-
-//set port
-app.set('port', process.env.PORT || 8080);
-// Use express to initalize graphqql
-app.use('/graphql', expressGraphQl({
-		schema: Schema,
-		rootValue: root,
-		graphiql: true
-	})
-);
-
-// Listen for the port
-app.listen(app.get('port'), () => {
-  console.log('Server started');
-});
+app.listen(5000, () => console.log('Server Running'))
