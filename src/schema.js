b49db7c4dfeb8e0a358355951dbdbb8023f5ba5d
Glitch (harmless-military-blob) <none>
2020-08-28 14:00:48 +0000

    🎗🕴 Checkpoint ./src/schema.js:9085975/11 ./src/index.js:9085975/4

--- src/schema.js
+++ src/schema.js
@@ -3,10 +3,10 @@
 // ANCHOR we define the schema which are used to structure the data flow b/n routes
 const {buildSchema} = require('graphql');
 
-const schema = buildSchema(`
+const schema = buildSchema`
 type Query {
 	hello: String
 }
-`)
+`;
 
 module.exports = schema;
